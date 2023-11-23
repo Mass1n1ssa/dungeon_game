845ccc0112dfaea1250be957830d781b784ff3d5
AMANZOUGAGHENE Massinissa <m.amanzougaghene@ecole-ipssi.net>
2023-11-23 11:59:39 +0100

    second

--- game.php
+++ game.php
@@ -5,5 +5,37 @@
     Class Personnage{
         private $nom;
         private $pv;
-        private 
+        private $pa;
+        private $pd;
+        private $xp;
+        private $nv;
+        private $arme;
+
+        public function __construct($nom, $pv, $pa, $pd, $xp, $niveau){
+            $this->nom = $nom;
+            $this->pv = $pv;
+            $this->pa = $pa;
+            $this->pd = $pd;
+            $this->xp = $xp;
+            $this->nv = $nv;
+            $this->arme = $arme;
+        }
     }
+
+
+    Class Salle {
+        private $description;
+        private $monstre;
+        private $enigme;
+        private $piege;
+        private $marchand;
+        
+        public function __construct($description, $monstre, $enigme, $piege, $marchand){
+            $this->description = $description;
+            $this->monstre = $monstre;
+            $this->enigme = $enigme;
+            $this->piege = $piege;
+            $this->marchand = $marchand;
+        }
+    }
+    
---
+++ gameDAO.php
@@ -0,0 +1,11 @@
+<?php
+    Class DAO{
+        private $bdd;
+
+        public function __construct(){
+            $this->bdd = $bdd;
+        }
+
+        
+
+    }
